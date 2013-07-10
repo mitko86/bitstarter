5470aa82a351a202e4a1b167447d6813a54486cf
mitko86 <dimitrov86@yahoo.com>
2013-07-11 01:32:35 +0300

    modified web.js

--- web.js
+++ web.js
@@ -4,10 +4,15 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+ 
+  fs.readFile('index.html', 'utf8', function(err, data) {
+  	if (err) throw err;
+  	response.send(data);
+  });
 
-  data = new Buffer(1024);
-  fs.readFile('index.html',data);
-  response.send(data.toString());
+//  data = new Buffer(1024);
+//  fs.readFile('index.html',data);
+//  response.send(data.toString());
 );
 
 var port = process.env.PORT || 5000;
