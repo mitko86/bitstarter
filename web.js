b68b75aded91f2c6e2b55c3efa1f187c8f46a01f
mitko86 <dimitrov86@yahoo.com>
2013-07-11 01:38:51 +0300

    modified web.js

--- web.js
+++ web.js
@@ -1,19 +1,33 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
+
 app.get('/', function(request, response) {
- 
-  fs.readFile('index.html', 'utf8', function(err, data) {
-  	if (err) throw err;
-  	response.send(data);
-  });
+
+var fs = require("fs");
+var fileName = "index.html";
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+          response.send(data);
+          fs.close(fd);
+        });
+      });
+    });
+  }
+});
+
 
 //  data = new Buffer(1024);
 //  fs.readFile('index.html',data);
 //  response.send(data.toString());
-);
+});
 
 var port = process.env.PORT || 5000;
 
