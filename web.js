e56cfdf9fd01748895ff1810fce443ec08257c44
mitko86 <dimitrov86@yahoo.com>
2013-07-11 01:19:11 +0300

    modified web.js

--- web.js
+++ web.js
@@ -6,15 +6,12 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
   var data = new Buffer(1024);
-
-
-	fs.readFile('index.html', function (err, data) {
-	
-  }); 
+  fs.readFile('index.html',data);
   response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 );
